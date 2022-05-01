bac9ac264079bf4a2607f7d01d6ae9e85c0bc82f
bgh716 <kbyun@sfu.ca>
2022-05-01 16:19:46 -0700

    mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Mushroom:TEMPO@lao.zskci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("LAO").collection("maris");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
