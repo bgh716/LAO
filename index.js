e4360e62f1887b11faa2faddf3a77543a836a890
bgh716 <kbyun@sfu.ca>
2022-05-01 17:18:11 -0700

    data updated

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ const port = process.env.PORT || 3000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://Mushroom:TEMPO@lao.zskci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/*
 client.connect(err => {
   const collection = client.db("LAO").collection("maris");
   // perform actions on the collection object
   collection.findOne({id:"TEMPO"}, function(err, doc) {
-        assert.equal(null, err);
-        assert.equal(null, doc.id);
+        console.log(err);
+        console.log(doc);
   });
-  client.close();
+  //client.close();
 });
-
+*/
 app.get('/', (req, res) => {
-  res.send(doc);
+  client.connect(err => {
+    const collection = client.db("LAO").collection("maris");
+    // perform actions on the collection object
+    collection.findOne({id:"TEMPO"}, function(err, doc) {
+          console.log(err);
+          console.log(doc);
+          res.send(doc);
+    });
+    //client.close();
+  });
 })
 
 app.listen(port, () => {
